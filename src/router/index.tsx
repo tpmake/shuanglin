076696dc1288e1ca6074acec922184d479b4ad96
flashforge <2994355324@qq.com>
2026-05-11 14:10:50 +0800

    修复 Layout 组件导入路径大小写问题

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, Navigate, createHashRouter } from 'react-router-dom';
-import Layout from '@/components/Layout';
+import Layout from '@/components/layout';
 import Home from '@/pages/home';
 import About from '@/pages/about';
 import Dashboard from '@/pages/dashboard';
